e6fe043b0e0281ecb6b169fc51a502c9ab9cfa73
IgorK <gosha424@mail.ru>
2024-01-08 09:54:39 +0300

    Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine ("Веедите число N");
+int n = 10;
+int[] arr = new int[n];
+int i = 0;
+//while (i < n)
+while (i < arr.Length)
+{
+    arr[i] = i + 1;
+    //Console.WriteLine(arr[i]);
+    //Console.WriteLine(" ");
+    Console.Write($"{arr[i]} ");
+    i = i + 1;
+}
