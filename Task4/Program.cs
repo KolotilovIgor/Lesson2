e7663aba098f755f198d9e51ad4c03c15eabe7f0
IgorK <gosha424@mail.ru>
2024-01-08 10:28:44 +0300

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+int n = 5;
+int[] array = {2,4,7,3,9};
+int i = 0;
+int max = array[0];
+while (i < n)
+{
+  if(array[i] > max)
+  {
+    max = array[i];
+  }
+  i++;
+}
+Console.WriteLine(max);
